95479b359b3522d068a4e1343575e53640dfce5f
jbullard66 <151672763+jbullard66@users.noreply.github.com>
2023-12-08 22:03:10 -0800

    Update PowerAnalysis.m

--- PowerAnalysis.m
+++ PowerAnalysis.m
@@ -79,7 +79,6 @@ m4 = rho*(r4+wL)*wL*dL;
 m5 = rho*(r5+wL)*wL*dL;
 m6 = rho*(r6+wL)*wL*dL;
 
-
 % Calculate moments of inertia
 i1 = 0;
 i2 = (1/12)*m2*((wL^2)+(r2)^2);
@@ -109,7 +108,7 @@ yCoMr2 = 0.5*r2*sin(angle2);
 x1CoMr2 = -0.5*r2*sin(angle2);
 y1CoMr2 = 0.5*r2*cos(angle2);
 x2CoMr2 = -xCoMr2;
-y2CoMr2 = -yComr2;
+y2CoMr2 = -yCoMr2;
 CoMa2 = angle2;
 
 xCoMr3 = (r2*cos(angle2))+(0.5*r3*cos(angle3));
@@ -118,7 +117,7 @@ x1CoMr3 = (-r2*sin(angle2))-(0.5*theta31*r3*sin(angle3));
 y1CoMr3 = (r2*cos(angle2))-(0.5*theta31*r3*cos(angle3));
 x2CoMr3 = (-r2*cos(angle2))-(0.5*theta32*r3*sin(angle3))-(0.5*(theta31)^2*r3*cos(angle3));
 y2CoMr3 = (-r2*sin(angle2))+(0.5*theta32*r3*cos(angle3))-(0.5*(theta31)^2*r3*sin(angle3));
-CoMa3 = atan(yComr3/xCoMr3);
+CoMa3 = atan(yCoMr3/xCoMr3);
 
 
 xCoMr4 = (r1*cos(angle1))-(0.5*r4*cos(angle4));
@@ -127,7 +126,7 @@ x1CoMr4 = (0.5*r4*theta41*sin(angle4));
 y1CoMr4 = (-0.5*r4*theta41*cos(angle4));
 x2CoMr4 = (0.5*theta42*r4*sin(angle4))+(0.5*(theta41)^2*r4*cos(angle4));
 y2CoMr4 = (-0.5*theta42*r4*cos(angle4))+(0.5*(theta41)^2*r4*sin(angle4));
-CoMa4 = atan((yComr4)/(xCoMr4));
+CoMa4 = atan((yCoMr4)/(xCoMr4));
 
 xCoMr5 = (r1*cos(angle1))-(r15*cos(angle4))-(0.5*r5*cos(angle5));
 yCoMr5 = (r1*sin(angle1))-(r15*sin(angle4))-(0.5*r5*sin(angle5));
@@ -135,16 +134,16 @@ x1CoMr5 = (r15*theta41*sin(angle4))+(0.5*r5*theta51*sin(angle5));
 y1CoMr5 = (-r15*theta41*cos(angle4))-(0.5*r5*theta51*cos(angle5));
 x2CoMr5 = (r15*theta42*sin(angle4))+(r15*(theta41)^2*cos(angle4))+(0.5*r5*theta52*sin(angle5))+(0.5*r5*(theta51)^2*cos(angle5));
 y2CoMr5 = (-r15*theta42*cos(angle4))+(r15*(theta41)^2*sin(angle4))-(0.5*r5*theta52*cos(angle5))+(0.5*r5*(theta51)^2*sin(angle5));
-CoMa5 = atan(yComr5/xCoMr5);
+CoMa5 = atan(yCoMr5/xCoMr5);
 
 
-xCoMr6 = (r1*cos(angle1))-(r15*cos(angle4))-(r5*cos(angle5))-(0.5*r6*cos(angle6))+(r66cos(angle6+pi));
-yCoMr6 = (r1*sin(angle1))-(r15*sin(angle4))-(r5*sin(angle5))-(0.5*r6*sin(angle6))+(r66sin(angle6+pi));
-x1CoMr5 = (r15*theta41*sin(angle4))+(r5*theta51*sin(angle5))+(0.5*r6*theta61*sin(angle6))-(r66*theta61*sin(angle6+pi));
-y1CoMr6 = (-r15*theta41*cos(angle4))-(r5*theta51*cos(angle5))-(0.5*r6*theta61*cos(angle6))+(r66*theta61*cos(angle6+pi));
-x2CoMr6 = (r15*theta42*sin(angle4))+(r15*(theta41)^2*cos(angle4))+(r5*theta52*sin(angle5))+(r5*(theta51)^2*cos(angle5))+(0.5*r6*theta62*sin(angle6))+(0.5*r6*(theta61)^2*cos(angle6))-(r66*theta62*sin(angle6+pi))-(r66*(theta61)^2*cos(angle6+pi));
-y2CoMr6 = (-r15*theta42*cos(angle4))+(r15*(theta41)^2*sin(angle4))-(r5*theta52*cos(angle5))+(r5*(theta51)^2*sin(angle5))-(0.5*r6*theta62*cos(angle6))+(0.5*r6*(theta61)^2*sin(angle6))+(r66*theta62*cos(angle6+pi))-(r66*(theta61)^2*sin(angle6+pi));
-CoMa6 = atan(yComr6/xCoMr6);
+xCoMr6 = (r1*cos(angle1))-(r15*cos(angle4))-(r5*cos(angle5))-(0.5*r6*cos(angle6))+(r6*cos(angle6+pi));
+yCoMr6 = (r1*sin(angle1))-(r15*sin(angle4))-(r5*sin(angle5))-(0.5*r6*sin(angle6))+(r6*sin(angle6+pi));
+x1CoMr6 = (r15*theta41*sin(angle4))+(r5*theta51*sin(angle5))+(0.5*r6*theta61*sin(angle6))-(r6*theta61*sin(angle6+pi));
+y1CoMr6 = (-r15*theta41*cos(angle4))-(r5*theta51*cos(angle5))-(0.5*r6*theta61*cos(angle6))+(r6*theta61*cos(angle6+pi));
+x2CoMr6 = (r15*theta42*sin(angle4))+(r15*(theta41)^2*cos(angle4))+(r5*theta52*sin(angle5))+(r5*(theta51)^2*cos(angle5))+(0.5*r6*theta62*sin(angle6))+(0.5*r6*(theta61)^2*cos(angle6))-(r6*theta62*sin(angle6+pi))-(r6*(theta61)^2*cos(angle6+pi));
+y2CoMr6 = (-r15*theta42*cos(angle4))+(r15*(theta41)^2*sin(angle4))-(r5*theta52*cos(angle5))+(r5*(theta51)^2*sin(angle5))-(0.5*r6*theta62*cos(angle6))+(0.5*r6*(theta61)^2*sin(angle6))+(r6*theta62*cos(angle6+pi))-(r6*(theta61)^2*sin(angle6+pi));
+CoMa6 = atan(yCoMr6/xCoMr6);
 
 
 %  Given constant omega
@@ -160,13 +159,13 @@ alpha2 = -w^2 / 2 * (thetamax - thetamin) * sin(w * t);
 % T2 = sum(Aj * alpha2) + sum(Bj * w2) + sum(mj * g * y1CoMrj)
 
 % Calculate the Change in Kinetic Energy dT/dt 
-A2 = Aj(m2,x1CoMr2,y1CoMr2, i2, theta21);
+A2 = Aj(m2,x1CoMr2,y1CoMr2, i2, 1);
 A3 = Aj(m3,x1CoMr3,y1CoMr3, i3, theta31);
 A4 = Aj(m4,x1CoMr4,y1CoMr4, i4, theta41);
 A5 = Aj(m5,x1CoMr5,y1CoMr5, i5, theta51);
 A6 = Aj(m6,x1CoMr6,y1CoMr6, i6, theta61);
 
-B2 = Bj(m2, x1CoMr2, x2CoMr2, y1CoMr2, y2CoMr2, i2, theta21, theta22);
+B2 = Bj(m2, x1CoMr2, x2CoMr2, y1CoMr2, y2CoMr2, i2, 1, 0);
 B3 = Bj(m3, x1CoMr3, x2CoMr3, y1CoMr3, y2CoMr3, i3, theta31, theta32);
 B4 = Bj(m4, x1CoMr4, x2CoMr4, y1CoMr4, y2CoMr4, i4, theta41, theta42);
 B5 = Bj(m5, x1CoMr5, x2CoMr5, y1CoMr5, y2CoMr5, i5, theta51, theta52);
@@ -179,18 +178,20 @@ dTdt = A + B;
 
 % Calculate the Change in Gravitational Potential Energy dU/dt
 
-Ugrav2 = m2 * 9.81 * y1CoM2;
-Ugrav3 = m3 * 9.81 * y1CoM3;
-Ugrav4 = m4 * 9.81 * y1CoM4;
-Ugrav5 = m5 * 9.81 * y1CoM5;
-Ugrav6 = m6 * 9.81 * y1CoM6;
-dUdt= Ugrav2 + Ugrav3 + Ugrav4 + Ugrav5 + Ugrav6;
+Ugrav2 = m2 * 9.81 * y1CoMr2;
+Ugrav3 = m3 * 9.81 * y1CoMr3;
+Ugrav4 = m4 * 9.81 * y1CoMr4;
+Ugrav5 = m5 * 9.81 * y1CoMr5;
+Ugrav6 = m6 * 9.81 * y1CoMr6;
+dUdt= (Ugrav2 + Ugrav3 + Ugrav4 + Ugrav5 + Ugrav6) * omega2;
 
 % Calculate Torque at a Time t
 
 torque = dUdt + dTdt;
 
 end
+
+
 
 
 
